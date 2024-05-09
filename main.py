94c6f9c9204814d881450698caa67db5df240304
imvenkata <venkat2200@gmail.com>
2024-05-09 13:42:37 +0100

    updated main.py file

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ import os
 import openai
 import pandas as pd
 from llama_index.experimental.query_engine import PandasQueryEngine
-from prompts import instruction_str, new_prompt
+from src.prompts import instruction_str, new_prompt, context
+from src.note_engine import note_engine
+from llama_index.core.tools import QueryEngineTool, ToolMetadata
+from llama_index.core.agent.legacy.react.base import ReActAgent
+from llama_index.llms.openai import OpenAI
 
+# API KEY
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
@@ -16,4 +21,21 @@ movie_query_engine = PandasQueryEngine(
 
 movie_query_engine.update_prompts({"pandas_prompt": new_prompt})
 
-movie_query_engine.query("What are the top 10 movies?")
+tools = [
+    note_engine,
+    QueryEngineTool(
+        query_engine=movie_query_engine,
+        metadata=ToolMetadata(
+            name="movies_df",
+            description="This gives information about movie data",
+        ),
+    ),
+]
+
+
+llm = OpenAI(model="gpt-3.5-turbo")
+agent = ReActAgent.from_tools(llm=llm, tools=tools, verbose=True, context=context)
+
+while (prompt := input("Enter a prompt (q to quit): ")) != "q":
+    response = agent.query(prompt)
+    print(response)
--- src/note_engine.py
+++ src/note_engine.py
@@ -1,18 +1,19 @@
-from llama_index.tools import FunctionTool
+from llama_index.core.tools import FunctionTool
 import os
 
-note_file = os.path.join(os.path.dirname(__file__), "data", "note.txt")
+note_file = os.path.join("data", "note.txt")
+
 
 def save_note(note: str):
     if not os.path.exists(note_file):
-        with open(note_file, "w") as f:
-        
+        open(note_file, "w")
+
     with open(note_file, "a") as f:
         f.writelines([note + "\n"])
     return "note saved"
 
 
-note_engine = FunctionTool(
+note_engine = FunctionTool.from_defaults(
     fn=save_note,
     name="note_saver",
     description="Save a note to a file",
